aafd9f3963ae7ee05ea1903b96e7613542542b44
Everton Maia <maia.everton22@gmail.com>
2022-05-17 14:25:51 -0300

    :tada:Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -28,7 +28,7 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
     const newTasks = tasks.map(task => task.id == id ? {
       ...task,
       isComplete: !task.isComplete
@@ -37,7 +37,7 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
     const filteredTasks = tasks.filter(task => task.id !== id);
     setTasks(filteredTasks);
   }
